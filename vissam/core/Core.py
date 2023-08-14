54adef963186540a6e821d207293c564a2fb6f36
armedHeistKino <97109907+armedHeistKino@users.noreply.github.com>
2023-08-14 20:47:47 +0900

    0814 어플리케이션 윈도우 초기화 및 설정

--- vissam/core/Core.py
+++ vissam/core/Core.py
@@ -1,6 +1,8 @@
 """
 
 """
+import json
+
 
 import glfw
 from OpenGL.GL import *
@@ -12,23 +14,69 @@ class Core:
     
     """
     
-    def __init__(self, config_path: str) -> None:
-        if not type(config_path) == str: raise TypeError()
+    def __init__(self, config_path: str = "") -> None:
+        """
+            __init__
+                A function initializes glfw context and generate window object. 
+        """
 
-        self._load_config()
-        self._retrieve_config()
+        if not glfw.init(): raise Exception()
 
-    def _load_config(self):
+        glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
+        glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+        glfw.window_hint(glfw.OPENGL_PROFILE , glfw.OPENGL_CORE_PROFILE)
+        glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, True)
+        glfw.window_hint(glfw.SAMPLES, 4)
+
+        self.application_window_width = 1280
+        self.application_window_height = 720
+        self.application_window_title = "My title"
+
+        self.application_window = glfw.create_window(
+            self.application_window_width, 
+            self.application_window_height,
+            self.application_window_title, 
+            None,
+            None
+        )
+        
+        self._set_application_middle(self.application_window)
+        
+        glfw.make_context_current(self.application_window)
+
+    def _set_application_middle(self, window = None):
+        """
+        
+        """
+        current_monitor = glfw.get_primary_monitor()
+        video_mode = glfw.get_video_mode(current_monitor)
+
+        glfw.set_window_pos(
+            window, 
+            video_mode.size.width // 2 - self.application_window_width // 2, 
+            video_mode.size.height // 2 - self.application_window_height // 2
+        )
+
+
+    def _load_config(self) -> str:
         ...
 
-    def _retrieve_config(self):
+    def _retrieve_config(self) -> None:
         ...
 
-    def _startup(self):
-        ...
+    def startup(self):
+        while not glfw.window_should_close(self.application_window):
+            glfw.poll_events()
+            glfw.swap_buffers(self.application_window)
 
-    def _load_shader(self, shader_path: str):
+    def _load_shader(self):
         ...
 
     def _attach_shader(self):
         ...
+
+    def __del__(self):
+        glfw.terminate()
+
+prog = Core()
+prog.startup()
